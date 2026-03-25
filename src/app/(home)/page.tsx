a66ecad8482dfe70c825a803640ab8cb4ee8b1ca
Volodymyr Vynnychuk <volodymyr.vynnychuk881@gmail.com>
2026-03-25 11:03:09 -0400

    add FAQ component , add Tailwind color to global.css

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,7 @@ import Contact from "../components/Contact";
 import Hero from "../components/Hero";
 import Services from "../components/Services";
 import Brand from "../components/Brand";
+import FAQ from "../components/FAQ";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Services></Services>
       <Contact></Contact>
       <Brand></Brand>
+      <FAQ></FAQ>
     </main>
       
       
---
+++ src/app/components/FAQ.tsx
@@ -0,0 +1,120 @@
+import Image from "next/image";
+import { Plus } from "lucide-react";
+export default function FAQ() {
+  return (
+    <>
+      <section className="bg-primary py-12">
+        <div>
+        <div className="mx-auto max-w-4xl px-4">
+          {/* Picture */}
+          <div className="w-full overflow-hidden rounded-xl">
+            <Image
+              src="/FAQ.jpg"
+              alt="Cosmetology service"
+              width={600}
+              height={400}
+              className="w-full h-[360px] object-cover"
+            />
+          </div>
+        </div>
+        <div className="mx-auto max-w-4xl px-4 py-12 ">
+          <h2 className="text-start text-2xl font-normal uppercase tracking-widest ">Mosk asked question</h2>
+        </div>
+
+        {/* ALl Question */}
+        <div className="mx-auto max-w-4xl px-4 ">
+          <div className="border-b border-gray-300 border-t">
+          <button className="w-full flex justify-between items-center py-4 text-left">
+            <span>
+              Question 1
+            </span> 
+                  <Plus>
+                    size={20}
+                  </Plus>
+                 
+          </button>
+        </div>
+
+        </div>
+
+         <div className="mx-auto max-w-4xl px-4 ">
+          <div className="border-b border-gray-300 ">
+          <button className="w-full flex justify-between items-center py-4 text-left">
+            <span>
+              Question 1
+            </span> 
+                  <Plus>
+                    size={20}
+                  </Plus>
+                 
+          </button>
+        </div>
+
+        </div>
+
+        <div className="mx-auto max-w-4xl px-4 ">
+          <div className="border-b border-gray-300 ">
+          <button className="w-full flex justify-between items-center py-4 text-left">
+            <span>
+              Question 1
+            </span> 
+                  <Plus>
+                    size={20}
+                  </Plus>
+                 
+          </button>
+        </div>
+
+        </div>
+
+        <div className="mx-auto max-w-4xl px-4 ">
+          <div className="border-b border-gray-300 ">
+          <button className="w-full flex justify-between items-center py-4 text-left">
+            <span>
+              Question 1
+            </span> 
+                  <Plus>
+                    size={20}
+                  </Plus>
+                 
+          </button>
+        </div>
+
+        </div>
+
+        <div className="mx-auto max-w-4xl px-4 ">
+          <div className="border-b border-gray-300 ">
+          <button className="w-full flex justify-between items-center py-4 text-left">
+            <span>
+              Question 1
+            </span> 
+                  <Plus>
+                    size={20}
+                  </Plus>
+                 
+          </button>
+        </div>
+
+        </div>
+
+        <div className="mx-auto max-w-4xl px-4 ">
+          <div className="border-b border-gray-300 ">
+          <button className="w-full flex justify-between items-center py-4 text-left">
+            <span>
+              Question 1
+            </span> 
+                  <Plus>
+                    size={20}
+                  </Plus>
+                 
+          </button>
+        </div>
+
+        </div>
+
+        
+        </div>
+      </section>
+    </>
+  );
+}
