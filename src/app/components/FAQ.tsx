56f31195cad5d180e3eb9172647b2e99a0af61c1
Volodymyr Vynnychuk <volodymyr.vynnychuk881@gmail.com>
2026-04-10 13:39:56 -0400

    made FAQ section  accordion functionality

--- src/app/components/FAQ.tsx
+++ src/app/components/FAQ.tsx
@@ -1,6 +1,42 @@
+'use client'
 import Image from "next/image";
-import { Plus } from "lucide-react";
+import { Plus, X } from "lucide-react";
+import { useState } from "react";
+type Item = {
+  title: string,
+  answer:string
+}
+
 export default function FAQ() {
+
+
+  const questions: Item[] = [
+    {
+      title: "Question 1",
+      answer: "Texts are any communication forms designed Texts are any communication forms designed Texts are any communication forms designed"
+    },
+    {
+      title: "Question 2",
+      answer: "Texts are any communication forms designed Texts are any communication forms designed Texts are any communication forms designed"
+    },
+    {
+      title: "Question 3",
+      answer: "Texts are any communication forms designed Texts are any communication forms designed Texts are any communication forms designed"
+    },
+    {
+      title: "Question 4",
+      answer: "Texts are any communication forms designed Texts are any communication forms designed Texts are any communication forms designed"
+    },
+  ]
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+  function openMenu(index: number) {
+    if (activeIndex === index) {
+      setActiveIndex(null);
+    } else {
+      setActiveIndex(index);
+    }
+  }
   return (
     <>
       <section className="bg-primary py-12">
@@ -22,94 +58,33 @@ export default function FAQ() {
 
         {/* ALl Question */}
         <div className="mx-auto max-w-4xl px-4 ">
-          <div className="border-b border-gray-300 border-t">
-          <button className="w-full flex justify-between items-center py-4 text-left">
-            <span>
-              Question 1
-            </span> 
-                  <Plus>
+
+          {questions.map((question, index) => (
+            <div className="border-b border-gray-300 " key={index}>
+
+              <button className="w-full flex justify-between items-center py-4 text-left" onClick={() => openMenu(index)} >
+                <span>
+                  {question.title}
+                </span>
+                {activeIndex === index
+                  ? <X>
                     size={20}
-                  </Plus>
-                 
-          </button>
-        </div>
-
-        </div>
-
-         <div className="mx-auto max-w-4xl px-4 ">
-          <div className="border-b border-gray-300 ">
-          <button className="w-full flex justify-between items-center py-4 text-left">
-            <span>
-              Question 1
-            </span> 
-                  <Plus>
+                  </X>
+                  : <Plus>
                     size={20}
-                  </Plus>
-                 
-          </button>
-        </div>
+                  </Plus>}
+              </button>
+              {activeIndex === index && (
+                <div className="pb-6 leading-7 mt-2 max-w-md">
+                  <p className="">{question.answer}</p>
+                </div>
+              )}
+            </div>
+          ))}
 
         </div>
 
-        <div className="mx-auto max-w-4xl px-4 ">
-          <div className="border-b border-gray-300 ">
-          <button className="w-full flex justify-between items-center py-4 text-left">
-            <span>
-              Question 1
-            </span> 
-                  <Plus>
-                    size={20}
-                  </Plus>
-                 
-          </button>
-        </div>
 
-        </div>
-
-        <div className="mx-auto max-w-4xl px-4 ">
-          <div className="border-b border-gray-300 ">
-          <button className="w-full flex justify-between items-center py-4 text-left">
-            <span>
-              Question 1
-            </span> 
-                  <Plus>
-                    size={20}
-                  </Plus>
-                 
-          </button>
-        </div>
-
-        </div>
-
-        <div className="mx-auto max-w-4xl px-4 ">
-          <div className="border-b border-gray-300 ">
-          <button className="w-full flex justify-between items-center py-4 text-left">
-            <span>
-              Question 1
-            </span> 
-                  <Plus>
-                    size={20}
-                  </Plus>
-                 
-          </button>
-        </div>
-
-        </div>
-
-        <div className="mx-auto max-w-4xl px-4 ">
-          <div className="border-b border-gray-300 ">
-          <button className="w-full flex justify-between items-center py-4 text-left">
-            <span>
-              Question 1
-            </span> 
-                  <Plus>
-                    size={20}
-                  </Plus>
-                 
-          </button>
-        </div>
-
-        </div>   
       </section>
     </>
   );
