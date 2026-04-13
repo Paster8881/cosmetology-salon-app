adab2ea7ed5c937c3c4be2aa85a5571f94f1c030
Volodymyr Vynnychuk <volodymyr.vynnychuk881@gmail.com>
2026-04-13 15:26:41 -0400

    fix layout for services saction and add md breakpoint for this section

--- src/app/components/Services.tsx
+++ src/app/components/Services.tsx
@@ -3,10 +3,28 @@ import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import 'swiper/css';
 import 'swiper/css/pagination';
+import Container from "./ui/Container";
 
 export default function Services() {
   const services = [
     {
+      title: "Cosmetology",
+      description: "Gentle skincare for a natural glow.",
+      image: "/services/kosmetolog.jpg",
+      href: "/services",
+    },
+    {
+      title: "Facial Care",
+      description: "Deep hydration and skin renewal.",
+      image: "/services/kosmetolog.jpg",
+      href: "/services",
+    },
+    {
+      title: "Facial Care",
+      description: "Deep hydration and skin renewal.",
+      image: "/services/kosmetolog.jpg",
+      href: "/services",
+    }, {
       title: "Cosmetology",
       description: "Gentle skincare for a natural glow.",
       image: "/services/kosmetolog.jpg",
@@ -29,33 +47,42 @@ export default function Services() {
   return (
     <>
       <section className="bg-secondary py-16">
-        <div className="max-w-sm mx-auto ">
-          <div className="text-center mb-8">
+        <Container>
+          <div className="text-center mb-16">
             <a
               href="/services"
-              className="inline-block text-sm uppercase tracking-[0.2em] border-b border-gray-300  hover:opacity-90 transition "
+              className="inline-block font-cormorant leading-tight text-sm uppercase tracking-[0.2em] border-b border-gray-300 md:text-xl"
             >
               View All Services
             </a>
           </div>
+
           {/* 
         Photo */}
-          <ul className="mx-auto max-w-sm px-4 list-none">
+          <ul className="list-none">
             <Swiper
               slidesPerView={1.15}
-              spaceBetween={16}
-              className="mySwiper "
+              spaceBetween={20}
+              resistance={true}
+              
+              resistanceRatio={0.9}
+              grabCursor={true}
+              breakpoints={{
+                768: {
+                  slidesPerView: "auto",
+                },
+              }}
             >
               {services.map((service, index) => (
-                <SwiperSlide key={index}>
+                <SwiperSlide key={index} className="max-w-[320px]">
                   <li>
-                    <a href={service.href} className="group block bg-primary">
+                    <a href={service.href} className="group block bg-primary shadow-[0_6px_20px_rgba(0,0,0,0.05)] ">
                       <Image
                         src={service.image}
                         alt={service.title}
                         width={600}
                         height={750}
-                        className="w-full h-120 object-cover  transition duration-300 group-hover:brightness-110"
+                        className="w-full aspect-3/4 object-cover  transition duration-300 group-hover:brightness-110"
                       />
 
                       <h3 className="mt-4 text-3xl font-cormorant px-4">
@@ -70,14 +97,10 @@ export default function Services() {
                 </SwiperSlide>
               ))}
 
-
             </Swiper>
 
-
           </ul>
-
-
-        </div>
+        </Container>
       </section>
 
 
