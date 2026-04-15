9bb1cb48699d39c0924ca3fa7f003c38cd4a4ed1
Volodymyr Vynnychuk <volodymyr.vynnychuk881@gmail.com>
2026-04-15 19:24:02 -0400

    refactor(footer): add md breakpoint and clean up component structure

--- src/app/components/Fotter.tsx
+++ src/app/components/Fotter.tsx
@@ -1,94 +1,75 @@
-import { Clock6, Smartphone , MapPin } from "lucide-react"
+import { Clock6, Smartphone, MapPin, LucideIcon } from "lucide-react"
+import Container from "./ui/Container"
+type IconData = {
+  icon: LucideIcon,
+  title: string,
+  descriptions: string[]
+}
 export default function Footer() {
+  const iconsData: IconData[] = [
+    {
+      icon: Clock6,
+      title: "Time",
+      descriptions: [
+        "Mon-Sat",
+        "From 8am To 5pm",
+      ]
+    },
+    {
+      icon: Smartphone,
+      title: "Contact",
+      descriptions: [
+        "P:+1 305 444 4444",
+        "Inst:@instexaple",
+        "Telegram:@teaxaple",
+      ]
+    },
+    {
+      icon: MapPin,
+      title: "Adress",
+      descriptions: [
+        " Key West",
+        "some adress exaple",
+      ]
+    }
+  ]
   return (
-    <section className="bg-secondary py-12">
-      <div className="mx-auto max-w-4xl px-4 flex flex-col items-center gap-15">
+    <section className="bg-secondary py-12 md:py-30">
+      <Container>
+        <div className="px-4 flex flex-col items-center gap-15 md:flex md:flex-row md:items-start md:gap-18">
 
-        <div className="flex flex-col items-center gap-1">
-          <div className="flex flex-col items-center">
-            <Clock6
-              width={34}
-              height={43}
-              stroke="black"
-              fill="#EAD0C9"
-            />
-          </div>
-          <h6 className="text-base font-semibold uppercase tracking-widest text-gray-700">Time</h6>
-          <div className="flex flex-col items-center gap-1">
-            <p className="flex gap-1.5">
-              <a className="ml-1.5" href="./">
-              Mon-Sat
-              </a>
-            </p>
-            <p className="flex gap-1.5">
-            <a className="ml-1.5">
-              From 8am To 5pm
-            </a>
-            </p>
-            
-          </div>
+          {iconsData.map((icon, index) => {
+            const Icon = icon.icon;
+            return (
+              <div className="flex flex-col items-center gap-2  md:gap-2" key={index}>
+                <div className="flex flex-col items-center  ">
+                  <Icon
+                    width={34}
+                    height={43}
+                    strokeWidth={1.5}
+                    fill="#EAD0C9"
+                    className="md:w-10 md:h-13"
+                  />
+                </div>
+                <h6 className="font-semibold uppercase tracking-widest text-gray-700 md:text-lg">{icon.title}</h6>
+                <div className="flex flex-col items-center gap-1 mt-1">
+                  {icon.descriptions.map((desciption , index) => (
+                    <div key={index}>
+                    <p className="tracking-widest text-base ">
+                    <a className="ml-1.5" href="./">
+                      {desciption}
+                    </a>
+                  </p>
+  
+                  </div>
+                  ))}
+                </div>
+              </div>
+            )
+          })}
+
         </div>
-
-        <div className="flex flex-col items-center gap-1">
-          <div className="flex flex-col items-center">
-            <Smartphone
-              width={34}
-              height={43}
-              stroke="black"
-              fill="#EAD0C9"
-            />
-          </div>
-          <h6 className="text-base font-semibold uppercase tracking-widest text-gray-700">Contact</h6>
-          <div className="flex flex-col items-center gap-1">
-            <p className="flex gap-1.5">
-              P:
-              <a className="ml-1.5" href="./">
-              +1 305 444 4444
-              </a>
-            </p>
-            <p className="flex gap-1.5">
-              Inst:
-            <a className="ml-1.5">
-              @instexaple
-            </a>
-            </p>
-
-            <p className="flex gap-1.5">
-              Telegram:
-            <a className="ml-1.5">
-              @teaxaple
-            </a>
-            </p>
-            
-          </div>
-        </div>
-
-        <div className="flex flex-col items-center gap-1">
-          <div className="flex flex-col items-center">
-            <MapPin
-              width={34}
-              height={43}
-              stroke="black"
-              fill="#EAD0C9"
-            />
-          </div>
-          <h6 className="text-base font-semibold uppercase tracking-widest text-gray-700">Adress</h6>
-          <div className="flex flex-col items-center gap-1">
-            <p className="flex gap-1.5">
-              <a className="ml-1.5" href="./">
-              Key West 
-              </a>
-            </p>
-            <p className="flex gap-1.5">
-            <a className="ml-1.5">
-              some adress exaple
-            </a>
-            </p>
-            
-          </div>
-        </div>
-
-      </div>
+      </Container>
     </section>
   )
 }
