71c23e3f8014d98675e3ff8e20602a0a902d071c
Volodymyr Vynnychuk <volodymyr.vynnychuk881@gmail.com>
2026-04-15 15:10:43 -0400

    add breckpoint for contacnt section and cleand code for this section

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -1,56 +1,59 @@
-import { Phone, Send, MessageCircle } from "lucide-react";
+import { Phone, Send, MessageCircle, LucideIcon } from "lucide-react";
+import Container from "./ui/Container";
+type IconsData = {
+  title: string,
+  icon: LucideIcon
+}
 export default function Contact() {
+  const IconsData: IconsData[] = [
+    {
+      title: "Viber",
+      icon: Phone
+    },
+    {
+      title: "Telegram",
+      icon: Send
+    },
+    {
+      title: "WhatsApp",
+      icon: MessageCircle
+    },
+  ]
   return (
     <section className="py-16 bg-primary md:py-24">
-      <div className="mx-auto max-w-4xl px-4 flex flex-col items-center gap-6">
+      <Container>
         <div className="flex flex-col items-center gap-4 text-center">
-          <h3 className=" text-2xl  uppercase tracking-widest font-cormorant font-medium">Feel free to call us</h3>
-          <p className="font-cormorant text-center   text-neutral-800 text-xl" >We are always happy to help you book or answer any questions</p>
+          <h3 className=" text-2xl  uppercase tracking-widest font-cormorant font-medium md:text-3xl mb-2">Feel free to call us</h3>
+          <p className="font-cormorant text-center    text-neutral-800 text-xl md:text-2xl" >We are always happy to help you book or answer any questions</p>
           <a
             href="tel:+13054444444"
-            className="text-4xl md:text-5xl font-medium tracking-wide transition hover:text-[#a87c7c]"
+            className="text-4xl md:text-5xl font-medium tracking-wide md:tracking-wider transition hover:text-[#a87c7c] mt-2"
           >
             +1 305 444 4444
           </a>
         </div>
 
 
-        <div className="flex justify-center gap-8">
+        <div className="flex justify-center gap-16 pt-12">
 
-          {/* Viber */}
-          <a href="#" className="flex flex-col items-center gap-2 group">
-            <div className="w-14 h-14 flex items-center justify-center rounded-full border border-[#D6BFB8] text-[#A88989] transition-colors duration-300 group-hover:bg-[#A88989] group-hover:text-white group-hover:shadow-md">
-              <Phone 
-              size={22}
-              strokeWidth={1.5}
-              />
-            </div>
-            <span className="text-sm text-neutral-500 transition-colors duration-300 group-hover:text-[#A88989]">Viber</span>
-          </a>
+          {IconsData.map((icon, index) => {
+            const Icon = icon.icon;
+            return (
+              <a href="#" className="flex flex-col items-center gap-2 group" key={index}>
+                <div className="w-14 h-14 flex items-center justify-center rounded-full border border-[#c79d90] text-[#A88989] transition-colors duration-300 group-hover:bg-[#A88989] group-hover:text-white group-hover:shadow-md">
 
-          {/* Telegram */}
-          <a href="#" className="flex flex-col items-center gap-2 group">
-            <div className="w-14 h-14 flex items-center justify-center rounded-full border border-[#D6BFB8] text-[#A88989] transition-colors duration-300 group-hover:bg-[#A88989] group-hover:text-white group-hover:shadow-md">
-              <Send size={24}
-                strokeWidth={1.5}
-              />
-            </div>
-            <span className="text-sm text-neutral-500 transition-colors duration-300 group-hover:text-[#A88989]">Telegram</span>
-          </a>
+                  <Icon className="w-6 h-6 md:w-8 md:h-8"
+                    strokeWidth={1.5}
+                  />
+                </div>
+                <span className="text-sm tracking-wide text-gray-700 transition-colors duration-300 group-hover:text-[#A88989]">{icon.title}</span>
+              </a>
+            )
+          })}
 
-          {/* WhatsApp */}
-          <a href="#" className="flex flex-col items-center gap-2 group">
-            <div className="w-14 h-14 flex items-center justify-center rounded-full border border-[#D6BFB8] text-[#A88989] transition-colors duration-300 group-hover:bg-[#A88989] group-hover:text-white group-hover:shadow-md">
-              <MessageCircle
-                size={24}
-                strokeWidth={1.5}
-              />
-            </div>
-            <span className="text-sm text-neutral-500 transition-colors duration-300 group-hover:text-[#A88989]">WhatsApp</span>
-          </a>
 
         </div>
-      </div>
+      </Container>
     </section>
   );
 }
