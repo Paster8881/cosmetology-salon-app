41b3923c3361708914bfdc10d405d1fac9fa84b2
Volodymyr Vynnychuk <volodymyr.vynnychuk881@gmail.com>
2026-04-15 15:24:52 -0400

    add breackpoint for Brand saction , clear code for this section

--- src/app/components/Brand.tsx
+++ src/app/components/Brand.tsx
@@ -1,118 +1,34 @@
 import Image from "next/image";
+import Container from "./ui/Container";
+
 export default function Brand() {
+  const iconsData: string[] = [
+    "/brands/1.svg", "/brands/1.svg", "/brands/1.svg", "/brands/1.svg", "/brands/1.svg", "/brands/1.svg", "/brands/1.svg", "/brands/1.svg"
+  ]
   return (
     <section className="bg-secondary py-14 px-6">
-      <div className="max-w-md mx-auto">
-        <div className="mx-auto max-w-4xl px-2 py-2">
-          <h2 className="text-center text-2xl  uppercase tracking-widest font-cormorant font-medium">Trusted brands of premium quality </h2>
+      <Container>
+        <div className="px-2 py-2">
+          <h2 className="text-center text-2xl  uppercase tracking-widest font-cormorant font-medium md:text-3xl">Trusted brands of premium quality </h2>
         </div>
 
-        <div className="grid grid-cols-2 gap-x-8 gap-y-10 mt-10 items-center">
-          <div className="flex justify-center">
-            <a href="">
-              <Image
-                src="/brands/1.svg"
-                alt="Cosmetology service"
-                width={160}
-                height={60}
-                className="grayscale opacity-50"
-              />
-
-            </a>
-          </div>
-
-          <div className="flex justify-center">
-            <a href="">
-              <Image
-                src="/brands/1.svg"
-                alt="Cosmetology service"
-                width={160}
-                height={60}
-                className="grayscale opacity-50"
-              />
-
-            </a>
-          </div>
-
-          <div className="flex justify-center">
-            <a href="">
-              <Image
-                src="/brands/1.svg"
-                alt="Cosmetology service"
-                width={160}
-                height={60}
-                className="grayscale opacity-50"
-              />
-
-            </a>
-          </div>
-
-          <div className="flex justify-center">
-            <a href="">
-              <Image
-                src="/brands/1.svg"
-                alt="Cosmetology service"
-                width={160}
-                height={60}
-                className="grayscale opacity-50"
-              />
-
-            </a>
-          </div>
-
-          <div className="flex justify-center">
-            <a href="">
-              <Image
-                src="/brands/1.svg"
-                alt="Cosmetology service"
-                width={160}
-                height={60}
-                className="grayscale opacity-50"
-              />
-
-            </a>
-          </div>
-
-          <div className="flex justify-center">
-            <a href="">
-              <Image
-                src="/brands/1.svg"
-                alt="Cosmetology service"
-                width={160}
-                height={60}
-                className="grayscale opacity-50"
-              />
-
-            </a>
-          </div>
-          <div className="flex justify-center">
-            <a href="">
-              <Image
-                src="/brands/1.svg"
-                alt="Cosmetology service"
-                width={160}
-                height={60}
-                className="grayscale opacity-50"
-              />
-
-            </a>
-          </div>
-          <div className="flex justify-center">
-            <a href="">
-              <Image
-                src="/brands/1.svg"
-                alt="Cosmetology service"
-                width={160}
-                height={60}
-                className="grayscale opacity-50"
-              />
-
-            </a>
-          </div>
+        <div className="grid grid-cols-2 gap-x-6 gap-y-8 mt-8 items-center">
+          {iconsData.map((icon, index) => (
+            <div className="flex justify-center" key={index}>
+              <a href="">
+                <Image
+                  src={icon}
+                  alt="Cosmetology service"
+                  width={160}
+                  height={60}
+                  className="grayscale opacity-50 hover:opacity-100 transition"
+                />
+              </a>
+            </div>
+          ))}
 
         </div>
-
-      </div>
+      </Container>
     </section>
   );
 }
