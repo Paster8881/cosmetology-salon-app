f92259fc39bf751dd92dc84aecb9f33ab7ce2917
Volodymyr Vynnychuk <volodymyr.vynnychuk881@gmail.com>
2026-04-17 12:13:08 -0400

    add lg breackpong for brand section

--- src/app/components/Brand.tsx
+++ src/app/components/Brand.tsx
@@ -6,13 +6,14 @@ export default function Brand() {
     "/brands/1.svg", "/brands/1.svg", "/brands/1.svg", "/brands/1.svg", "/brands/1.svg", "/brands/1.svg", "/brands/1.svg", "/brands/1.svg"
   ]
   return (
-    <section className="bg-secondary py-14 px-6">
+    <section className="bg-secondary py-14 px-6 lg:py-24">
       <Container>
-        <div className="px-2 py-2">
+        <div className="px-2 py-2 ">
           <h2 className="text-center text-2xl  uppercase tracking-widest font-cormorant font-medium md:text-3xl">Trusted brands of premium quality </h2>
         </div>
 
-        <div className="grid grid-cols-2 gap-x-6 gap-y-8 mt-8 items-center">
+        <div className="grid grid-cols-2 gap-x-6 gap-y-8 mt-8 items-center lg:grid-cols-4 lg:pt-10
+        ">
           {iconsData.map((icon, index) => (
             <div className="flex justify-center" key={index}>
               <a href="">
